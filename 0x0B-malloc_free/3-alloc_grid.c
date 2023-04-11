7ef87f7a1510540507b4ebb922875b02f80c2ea0
abdeljalilb <abdon.6524@gmail.com>
2023-04-10 21:59:50 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,56 +1,41 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * str_concat - function that concatenates two strings
- *
- * @s1: string of chars
- * @s2: string of chars
- *
- * Return: address of the newly allocated memory
+ * **alloc_grid - function to allocate memory to grid
+ * @width: int type
+ * @height: int type
+ * Return: grid of 0s
  */
-
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	unsigned int len1, len2;
-	unsigned int i, j;
-	char *str_copy;
-	char *tmp1 = s1;
-	char *tmp2 = s2;
+	int x, y;
+	int **ptr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	i = 0;
-	while (*s1++)
-		i++;
-	len1 = i;
-	s1 = tmp1;
-
-	i = 0;
-	while (*s2++)
-		i++;
-	len2 = i;
-	s2 = tmp2;
-
-	str_copy = malloc((len1 + len2) * sizeof(char) + 1);
-	if (str_copy == NULL)
+	if (width <= 0 || height <= 0)
+	{
+		return  (NULL);
+	}
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+	{
 		return (NULL);
-
-	j = 0;
-	while (j < len1)
-	{
-		str_copy[j] = s1[j];
-		j++;
 	}
-	while (j < len1 + len2)
+	for (x = 0; x < height; x++)
 	{
-		str_copy[j] = s2[j - len1];
-		j++;
+		ptr[x] = malloc(width * sizeof(int));
+		if (ptr[x] == NULL)
+		{
+			for (y = 0; y < x;  y++)
+				free(ptr[y]);
+			free(ptr);
+			return (NULL);
+		}
+		for (y = 0; y < width; y++)
+		{
+			ptr[x][y] = 0;
+		}
 	}
-	str_copy[j] = '\0';
-	return (str_copy);
+	return (ptr);
 }
